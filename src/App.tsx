01a7e9cfce652deca0537f947998b6a2c689fec1
Cigaro <dimagilew@mail.ru>
2022-02-01 19:45:53 +0300

    fix material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
-  return (<h1>Hi</h1>);
+  return (
+    <>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </>
+  );
 }
 
 export default App;
